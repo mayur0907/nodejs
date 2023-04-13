b8de9c53fa2db7dcdf7330a28116f951231c1b78
mayur0907 <121195884+mayur0907@users.noreply.github.com>
2023-04-13 12:34:08 +0530

    Update play.js

--- play.js
+++ play.js
@@ -5,3 +5,20 @@ const getProduct = (num1, num2) => num1 * num2;
 
 
 console.log(getProduct(5, 7)); // Output: 35
+
+
+
+
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  console.log('Mayur');
+  res.send('Mayur');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${4000}`);
+});
+
